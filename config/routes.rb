56567fb4bb169686f03a86062d0faf7304a40b13
vva <vishaladepu007@gmail.com>
2024-02-06 14:49:58 -0600

    init

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root 'books#index'
+
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+  devise_scope :admin do
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end
+
+  resources :books
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Book.create!([
+  { title: '1' },
+  { title: '34' },
+  { title: '128' },
+  { title: '1285454' }
+]
+            )
