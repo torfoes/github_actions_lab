ae12c42a05cbf9b2f39047d2625d9164c1f8e481
karlos <karloszuru@gmail.com>
2024-09-08 14:10:34 -0500

    rspec testing

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  it 'has the correct number of seeded books' do
+    expect(Book.count).to eq(5)
+  end
+
+  it 'includes a specific seeded book' do
+    book = Book.find_by(title: "The life of a turtle")
+    expect(book).to be_present
+  end
+end
